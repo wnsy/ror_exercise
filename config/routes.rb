541f437ab1e57120e8ae75fbdec7817585eb8524
wnsy <ayesha.wnz@gmail.com>
2018-04-11 13:31:26 +1200

    Modifies routes for admins, businesses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+	devise_for :admins, path: 'admins', controllers: {
+		sessions: 'admins/sessions'
+	}
+	# eg. http://localhost:3000/admins/sign_in
+	devise_for :businesses, path: 'businesses', controllers: {
+			sessions: 'businesses/sessions'
+	}
+	# eg. http://localhost:3000/businesses/sign_in
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'application#hello'
 end
