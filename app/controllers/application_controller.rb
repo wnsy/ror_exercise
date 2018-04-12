96e5dfc03215f72cbc9ee0688f990a3e3a8e7b9f
wnsy <ayesha.wnz@gmail.com>
2018-04-12 18:33:58 +1200

    Overwrites sign_out redirect path method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
 
 	def hello
-		render html: "¡Hola, mundo!"
+		render html: "hello, world!"
 	end
 
-	def goodbye
-		render html: "goodbye, world!"
+	private
+
+	# Overwriting the sign_out redirect path method
+	def after_sign_out_path_for(resource_or_scope)
+		if resource_or_scope == :admin
+			new_admin_session_path
+		elsif resource_or_scope == :business
+			new_business_session_path
+		else
+			root_path
+		end
 	end
+
 end
