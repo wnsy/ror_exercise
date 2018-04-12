18890508bac41c5367f62f610bcafe0a5ce35603
wnsy <ayesha.wnz@gmail.com>
2018-04-12 18:36:23 +1200

    Adds resources, paths

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 	devise_for :businesses, path: 'businesses', controllers: {
 			sessions: 'businesses/sessions'
 	}
+
+	# namespace :admin do
+	resources :admins, :businesses
+	# end
+
 	# eg. http://localhost:3000/businesses/sign_in
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'application#hello'
