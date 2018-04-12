440e46259f3f8666d9ac055629e75ae4aa747bd2
wnsy <ayesha.wnz@gmail.com>
2018-04-12 21:56:54 +1200

    Adds cross models visits solution

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,23 +1,21 @@
 # ../controllers/concerns/accessible.rb
 module Accessible
-  extend ActiveSupport::Concern
-  included do
-    before_action :check_user
-  end
+	extend ActiveSupport::Concern
+	included do
+		before_action :check_user
+	end
 
-  protected
-  def check_user
-    if current_admin
-      flash.clear
-      # if you have rails_admin. You can redirect anywhere really
-      redirect_to(new_admin_session_path) && return
-			#redirect_to(rails_admin.dashboard_path) && return
-    elsif current_business
-      flash.clear
-      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+	protected
+	def check_user
+		if current_admin
+			flash.clear
+			redirect_to(new_admin_session_path) && return
+		elsif current_business
+			flash.clear
+			# The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
 			redirect_to(new_business_session_path) && return
-    end
-  end
+		end
+	end
 end
 
 #https://github.com/plataformatec/devise/wiki/How-to-Setup-Multiple-Devise-User-Models
