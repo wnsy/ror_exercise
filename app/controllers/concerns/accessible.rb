1133903f08818230896ee058617bc2dd47355be3
wnsy <ayesha.wnz@gmail.com>
2018-04-12 22:01:02 +1200

    Adds cross models visits solution

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,17 @@
+module Accessible
+  extend ActiveSupport::Concern
+  included do
+    before_action :check_user
+  end
+
+  protected
+  def check_user
+    if current_admin
+      flash.clear
+      redirect_to(new_admin_session_path) && return
+    elsif current_business
+      flash.clear
+			redirect_to(new_business_session_path) && return
+    end
+  end
+end
