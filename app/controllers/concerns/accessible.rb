d650f87539d1b99248a7381d9fcd0f45ca77ffb9
wnsy <ayesha.wnz@gmail.com>
2018-04-13 23:00:35 +1200

    Changes the redirected paths

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -8,10 +8,10 @@ module Accessible
   def check_user
     if current_admin
       flash.clear
-      redirect_to(new_admin_session_path) && return
+      redirect_to(admins_path) && return
     elsif current_business
       flash.clear
-      redirect_to(new_business_session_path) && return
+      redirect_to(businesses_path) && return
     end
   end
 end
